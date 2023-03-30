a861312ca61259e21d44a05ae4b5a1a2b1f58445
KongphopTongdee <kongphop.working@gmail.com>
2023-03-30 11:38:49 +0700

    little_UPdate

--- What_you_have_learned/Region_Growing/Region_Growing.py
+++ What_you_have_learned/Region_Growing/Region_Growing.py
@@ -1,3 +1,10 @@
 import cv2
 import numpy as np
 
+picture_for_region_growing = cv2.imread('Lenna.png', 0)
+
+print(picture_for_region_growing)
+
+cv2.imshow('test_image', picture_for_region_growing)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
