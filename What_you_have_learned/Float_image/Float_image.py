79d3dcae2b693388a58fc155220dfc5d978f5d88
KongphopTongdee <kongphop.working@gmail.com>
2023-04-19 10:07:12 +0700

    float_image

---
+++ What_you_have_learned/Float_image/Float_image.py
@@ -0,0 +1,19 @@
+import cv2
+
+picture_for_float_image = cv2.imread('Lenna.png')
+
+# print(picture_for_float_image)
+
+blue,green,red = cv2.split(picture_for_float_image)
+
+new_Blue = blue/255
+new_Green = green/255
+new_Red = red/255
+
+# print(ne_Blue)
+# print(new_Green)
+# print(new_Red)
+
+picture_for_new_float_image = cv2.merge((new_Blue, new_Green, new_Red))
+
+print(picture_for_new_float_image)
