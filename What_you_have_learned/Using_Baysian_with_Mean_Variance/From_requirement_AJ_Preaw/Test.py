677bf3c03f65a19c2ef40553858b03743ced7d49
KongphopTongdee <kongphop.working@gmail.com>
2023-04-28 01:31:02 +0700

    did_not_have_new_thing

--- What_you_have_learned/Using_Baysian_with_Mean_Variance/From_requirement_AJ_Preaw/Test.py
+++ What_you_have_learned/Using_Baysian_with_Mean_Variance/From_requirement_AJ_Preaw/Test.py
@@ -1,3 +1,16 @@
+import math
+
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+#   def myfunc(self):
+#     print("Hello my name is " + self.name)
+
+# p1 = Person("John", 36)
+# p1.myfunc()
+
 # store_swatch = [[],[],[],[],[],[],[],[],[],[]]
 
 # store_swatch[5].append(10)
@@ -23,9 +36,41 @@
 
 # print(ans)
 
-test = 111
+# test = 111
 
-for i in range(15):
-    test += 1
-    test = test%10
-    print(test)
+# for i in range(15):
+#     test += 1
+#     test = test%10
+#     print(test)
+
+# print(math.pi)
+
+# print(math.exp(1))
+
+# print('Hello world')
+
+calculate_mean_leaf = [128,185,111] 
+calculate_mean_outside_leaf_on_the_left= [168,163,151]
+calculate_mean_outside_leaf_on_the_right = [237,208,184]
+calculate_mean_outside_leaf_on_the_top = [129,119,107]
+calculate_mean_leaf_on_the_bottom = [185,140,114]
+
+# calculate mean leaf(RGB) = 128 185 111 = 141.34
+# calculate mean outside leaf on the left(RGB) = 168 163 151 = 160.67
+# calculate mean outside leaf on the right(RGB) = 237 208 184 = 209.67
+# calculate mean outside leaf on the top(RGB) = 129 119 107 = 118.33
+# calculate mean leaf on the bottom(RGB) = 185 140 114 = 146.34
+
+
+def calculate_mean(input):
+    Ans = 0.0
+    for i in input:
+        Ans += i
+    Ans = Ans / len(input)
+    return Ans
+
+print(calculate_mean(calculate_mean_leaf))
+print(calculate_mean(calculate_mean_outside_leaf_on_the_left))
+print(calculate_mean(calculate_mean_outside_leaf_on_the_right))
+print(calculate_mean(calculate_mean_outside_leaf_on_the_top))
+print(calculate_mean(calculate_mean_leaf_on_the_bottom))
--- What_you_have_learned/Using_Baysian_with_Mean_Variance/From_requirement_AJ_Preaw/Training_Bayesian_from_AJ_Preaw.py
+++ What_you_have_learned/Using_Baysian_with_Mean_Variance/From_requirement_AJ_Preaw/Training_Bayesian_from_AJ_Preaw.py
@@ -1,14 +1,44 @@
 import cv2 
-import csv
-import collections
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-def show_img(img):
+def show_img_cv2(img):
     cv2.imshow("test",img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    
+def show_img_matplotlib(img):
+    #use only matplotlib to plot the scale 
+    plt.imshow(img)
+    plt.show()
+
+def cal_prob_of_score_and_class(current_pixel, mean_of_class, std_of_class):
+    # equation 
+    # P(S|C) = (1/(standard_deviation*squareroot(2*pi)))(exp((-1/2)((current_pixel-mean)^2/standard_deviation)))
+    Ans = 0.0
+    
+    Ans = (1/(std_of_class*math.sqrt(2*math.pi)))*(math.exp((-1/2)*((math.pow((current_pixel-mean_of_class),2))/std_of_class)))
+    
+    return Ans
+
+# Calculate_Probability_Region_of_interesting
+def cal_prob_roi(interestsample,notinterestsample,prob_of_score_and_class,prob_of_score_and_notclass):
+    # equation 
+    # P(C|S) = 1/(1+((P(~C)*P(S|~C))/(P(C)*P(S|C))))
+    # เมื่อ P(C|S) = class ที่เราสนใจใน score นั้นๆ เช่น probability ของใบไม้(class)ใน mean(score)
+    # P(~C) = จำนวน sample ทั้งหมดที่ไม่ใช่ใบไม้(class)
+    # P(C) = จำนวน sample ทั้งหมดที่เป็นใบไม้(class)
+    # P(S|C) = likelihood เป็นค่า prob ที่เลือกใน distribution เช่น plot histogram ออกมาแล้วทำการหาว่าสีเขียวหรือสีที่เราต้องการใน distribution นั้นๆมีค่าเท่ากับเท่าไรแล้วนำมาหารด้วยทั้งหมด
+        #    = distribution class in mean Y, mean Cr, mean Cb and std Y, std Cr, Std Cb
+    # P(S|~C) = likelihood เป็นค่า prob ที่ไม่เลือกใน distribution เช่น plot histogram ออกมาแล้วทำการหาว่าสีเขียวหรือสีที่เราต้องการใน distribution นั้นๆมีค่าเท่ากับเท่าไรแล้วนำมาหารด้วยทั้งหมด
+        #    = distribution not class in mean Y, mean Cr, mean Cb and std Y, std Cr, Std Cb
+    Ans = 0.0
+    
+    Ans = 1/(1+((notinterestsample*prob_of_score_and_notclass)/(interestsample*prob_of_score_and_class)))
+    
+    return Ans
+
 
 # img_Leaf_tilted = cv2.imread("Leaf_tilted.png")
 img_Leaf_straight_line = cv2.imread("Leaf_straight_line.png")
@@ -17,98 +47,112 @@ height, width, channels = img_Leaf_straight_line.shape
 print("height and width of this picture " + "(" + str(height) +","+str(width) +","+str(channels) +")")
 
 img_convert_YCRCB = cv2.cvtColor(img_Leaf_straight_line, cv2.COLOR_BGR2YCR_CB)
+img_convert_RGB = cv2.cvtColor(img_Leaf_straight_line, cv2.COLOR_BGR2RGB)
+Y,Cr,Cb = cv2.split(img_convert_YCRCB)
 
 # cv2.rectangle(img_Leaf_straight_line, (445,470), (712,23), (0,0,255), 2)
 # show_img(img_Leaf_straight_line)
 # cv2.rectangle(img_Leaf_tilted, (445,470), (712,23), (0,0,255), 2)
 # show_img(img_Leaf_tilted)
 
-# พรุ่งนี้ตต้องทำมาลองสร้างค่าที่เก็บสี leaf ที่อยู่ใน position ที่เลือกแล้วทำการหา threshold บ้างค่าที่สามารถตีได้ว่าสีเขียวคือ threshold อะไร 
-# ก่อนที่จะนำไปเขียน probability
+# Step of working
+# 1. ทำการหา region growing เพื่อทำการตั้ง threshold ที่เอาเฉพาะค่าใบไม้มาใช้ โดยถ้าเป็นใบไม้จะให้เป็นสีขาวแต่ถ้าเป็นสิ่งที่ไม่สำคัญจะให้เป็นสีดำ
+# 2. ทำการหา interestsample,notinterestsample จากการนับเฉพาะสีขาวจาก region growing
+# 3. ทำการหา prob_of_score_and_class, prob_of_score_and_notclass โดยการคำนวน equation ที่ผนวกค่า current position, mean, standard deviation
+# 4. สุดท้ายนำไปใส่ในสมการ calculate bayesian probability ที่หา class(leaf)|score(mean,std of Y,Cr,Cb)
+
+# step 1. ทำการหา region growing เพื่อทำการตั้ง threshold ที่เอาเฉพาะค่าใบไม้มาใช้ โดยถ้าเป็นใบไม้จะให้เป็นสีขาวแต่ถ้าเป็นสิ่งที่ไม่สำคัญจะให้เป็นสีดำ
+# เนื่องจากว่าการ region growing ที่ง่ายที่สุดจะเป็นภาพขาวดำ
+region_growing_only = cv2.imread("Leaf_straight_line.png")
+region_growing_only_convert_RGB = cv2.cvtColor(region_growing_only, cv2.COLOR_BGR2RGB)
+# region_growing_only_convert_YCrCb = cv2.cvtColor(region_growing_only, cv2.COLOR_BGR2YCrCb)
+# region_growing_only_convert_YCr_Cb = cv2.cvtColor(region_growing_only, cv2.COLOR_BGR2YCR_CB)
+# region_growing_only = cv2.imread("Leaf_straight_line.png")
+
+cv2.rectangle(region_growing_only_convert_RGB, (445,470), (712,23), (0,0,255), 1)
+# position that is green is (x,y) = (530,300)
+# position outside leaf on the left = 490,280
+# position outside leaf on the right = 706,294
+# position outside leaf on the top = 582,96
+# position outside leaf on the bottom = 620,441
+
+# calculate mean leaf(YCrCb) = 166 105 100 = 123.67
+# calculate mean outside leaf on the left(YCrCb) = 167 132 120 = 139.67
+# calculate mean outside leaf on the right(YCrCb) = 234 139 114 = 162.34
+# calculate mean outside leaf on the top(YCrCb) = 115 134 120 = 123
+# calculate mean leaf on the bottom(YCrCb) = 139 155 107 = 133.67
+
+# calculate mean leaf(RGB) = 128 185 111 = 141.34
+# calculate mean outside leaf on the left(RGB) = 168 163 151 = 160.67
+# calculate mean outside leaf on the right(RGB) = 237 208 184 = 209.67
+# calculate mean outside leaf on the top(RGB) = 129 119 107 = 118.33
+# calculate mean leaf on the bottom(RGB) = 185 140 114 = 146.34
+
+# ดูจากการที่เราประมาณค่าสังเกตุได้ว่ามีบางค่า mean ที่ใกล้เคียงกันคือ mean leaf กับ mean bottom ทำให้ถ้าเลือกตัดค่า threshold นี้อาจจะมีปัญหาได้เพราะ ฉนั้นจะลองใช้ค่า
+# บางตัวจาก R หรือ G หรือ B เพื่อเป็นตัวมาตัด threshold ดั้งนั้นผลสรุปค่าที่ได้เลือกนำมาใช้ คือ ค่า
+
+show_img_matplotlib(region_growing_only_convert_RGB)
 
 
-# storing_color_value_list_row = []
-# storing_color_value_list_all = []
-
-# with open('color_value_rectangle_area.csv', 'w', encoding='UTF8', newline='') as f:
-#     writer = csv.writer(f)
-#     for height_of_picture in range(23,470):   # y position
-#         for width_of_picture in range(445,712):     # x position
-#             storing_color_value_list_row.append(img_Leaf_straight_line[height_of_picture][width_of_picture])
-#         # print(storing_color_value_list_row)
-#         storing_color_value_list_all.append(storing_color_value_list_row)
-#         storing_color_value_list_row = []
-
-#     print(storing_color_value_list_all)
-
-#     writer.writerows(storing_color_value_list_all)  
-#     storing_color_value_list_all = []  
 
 
-# cv2.rectangle(img_Leaf_straight_line, (445,470), (712,23), (0,0,255), 2)
-# show_img(img_Leaf_straight_line)
-
-set_leaf = []
-set_background = []
 
 
-# test code for know scale of pixel
-
-# Creating region selecting by rectangle
-position_start = (0,0)      #(width,hight)
-position_end = (0,0)        #(width,hight)
-count_click = 0
-seed_positon = (0,0)
-value_of_YCrCb = (0,0,0)
-Y,Cr,Cb = Y,Cr,Cb = cv2.split(img_convert_YCRCB)
-# Y,Cr,Cb = Y,Cr,Cb = cv2.split(img_Leaf_straight_line)
-
-#use only matplotlib to plot the scale 
-plt.imshow(img_convert_YCRCB)
-plt.show()
-# plt.imshow(Y)
-# plt.show()
-# plt.imshow(Cr)
-# plt.show()
-# plt.imshow(Cb)
-# plt.show()
 
 
-# not use
-# def Position_of_value(event,x,y,flags,param):
-#     global img_Leaf_straight_line,count_click, position_start, position_end, seed_positon, value_of_YCrCb
-    
-#     if (event == cv2.EVENT_LBUTTONDOWN):
-#         # if (count_click %2 == 0):
-#         #     position_start = (x,y)
-#         #     count_click += 1
-#         #     print("position_start" +"("+ str(x) +","+ str(y)+")")
-#         # else:
-#         #     position_end = (x,y)
-#         #     count_click += 1
-#         #     print("position_end" +"("+ str(x) +","+ str(y)+")")
-#         seed_positon = (x,y)
-#         value_of_YCrCb = (Y[x][y], Cr[x][y], Cb[x][y])
-#         print("seed_position " +"("+ str(x) +","+ str(y)+")")
-#         print(value_of_YCrCb)
-        
-            
-#     # elif(event == cv2.EVENT_RBUTTONDOWN):
-#     #     cv2.rectangle(img_Leaf_straight_line, position_start, position_end, (0,0,255), 2)
- 
- 
-# cv2.namedWindow('Leaf_straight_line')
-# cv2.setMouseCallback('Leaf_straight_line',Position_of_value) 
- 
 
-# # การคำนวนรูปภาพกับตัว show รูปภาพใช้คนละตัวกัน คำนวนใช้ color model = Y,Cr,Cb แต่ output รูปภาพใช้ R,G,B
 
-# while(1):
-#     cv2.imshow('Leaf_straight_line', img_Leaf_straight_line)
-#     k = cv2.waitKey(1) & 0xFF
-    
-#     if k == 27:
-#         break
 
-# cv2.destroyAllWindows()
+
+
+# step 2. ทำการหา interestsample,notinterestsample จากการนับเฉพาะสีขาวจาก region growing
+
+
+
+
+
+
+
+
+
+
+
+
+# step 3. ทำการหา prob_of_score_and_class, prob_of_score_and_notclass โดยการคำนวน equation ที่ผนวกค่า current position, mean, standard deviation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# step 4. สุดท้ายนำไปใส่ในสมการ calculate bayesian probability ที่หา class(leaf)|score(mean,std of Y,Cr,Cb)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
